fe9ff4af823fdf3713c5e61793e82662ae424575
FHainzl <fab.hainzl@utanet.at>
2017-07-15 16:23:29 +0200

    Add data loader for two backgrounds per encoder

--- launcher.py
+++ launcher.py
@@ -1,11 +1,13 @@
 from numpy import save as save_array
-from os.path import join as path_join
-from numpy import repeat
+from os.path import join as path_join, isfile
+from numpy import repeat, asarray
 from playground.utils.visualisation import plot_latent_2d, plot_sampled_data, plot_reconstructed_data
 from playground.model_trainer import ConjointVAEModelTrainer, ConjointAVBModelTrainer
 from playground.utils.datasets import load_conjoint_synthetic, load_mnist
 from playground.utils.logger import logger
 from keras.backend import clear_session
+from playground.utils.data_factory import CustomMNIST
+
 
 # import tensorflow as tf
 # from keras.backend.tensorflow_backend import set_session
@@ -71,16 +73,29 @@ def run_synthetic_experiment(model='avb', pretrained_model=None):
     return model_dir
 
 
-def run_mnist_experiment(model='avb', pretrained_model=None):
+def run_mnist_experiment(model='avb', pretrained_model=None, two_backgrounds_per_encoder=True):
     logger.info("Starting a conjoint model experiment on the MNIST Variations dataset.")
     data_dims = (784, 784)
     latent_dims = (2, 2, 2)
-    # data_0 = load_mnist(one_hot=False, binarised=False, background=None, rotated=False)
-    # data_1 = load_mnist(one_hot=False, binarised=False, background='image', rotated=False)
-    data_0 = load_mnist(local_data_path='data/MNIST_Custom_Variations/strippy_horizontal.npz',
-                        one_hot=False, binarised=False, background='custom')
-    data_1 = load_mnist(local_data_path='data/MNIST_Custom_Variations/strippy_vertical.npz',
-                        one_hot=False, binarised=False, background='custom')
+    if not two_backgrounds_per_encoder:
+        data_0 = load_mnist(local_data_path='data/MNIST_Custom_Variations/strippy_horizontal.npz',
+                            one_hot=False, binarised=False, background='custom')
+        data_1 = load_mnist(local_data_path='data/MNIST_Custom_Variations/strippy_vertical.npz',
+                            one_hot=False, binarised=False, background='custom')
+    elif two_backgrounds_per_encoder:
+        local_path_0 = 'data/MNIST_Custom_Variations/strippy_horizontal_and_vertical.npz'
+        local_path_1 = 'data/MNIST_Custom_Variations/trippy_and_mandelbrot.npz'
+        if not isfile(local_path_0):
+            cmnist = CustomMNIST()
+            new_data = cmnist.generate(1000, [0, 1, 0, 0])
+            cmnist.save_dataset(new_data, 'strippy_horizontal_and_vertical')
+        data_0 = load_mnist(local_path_0, one_hot=False, binarised=False, background='custom')
+        if not isfile(local_path_1):
+            cmnist = CustomMNIST()
+            new_data = cmnist.generate(1000, [0.5, 0, 0.5, 0])
+            cmnist.save_dataset(new_data, 'trippy_and_mandelbrot')
+        data_1 = load_mnist(local_path_1, one_hot=False, binarised=False, background='custom')
+
     train_data = ({'data': data_0['data'][:-100], 'target': data_0['target'][:-100]},
                   {'data': data_1['data'][:-100], 'target': data_1['target'][:-100]})
     test_data = ({'data': data_0['data'][-100:], 'target': data_0['target'][-100:]},
@@ -94,9 +109,7 @@ def run_mnist_experiment(model='avb', pretrained_model=None):
     elif model == 'avb':
         trainer = ConjointAVBModelTrainer(data_dims=data_dims, latent_dims=latent_dims, noise_dim=16,
                                           use_adaptive_contrast=False,
-                                          optimiser_params={'encdec': {'lr': 1e-4, 'beta_1': 0.5},
-                                                            'disc': {'lr': 2e-4, 'beta_1': 0.5}},
-                                          overwrite=True,
+                                          optimiser_params=None,
                                           pretrained_dir=pretrained_model,
                                           architecture='mnist',
                                           experiment_name='mnist_variations')
@@ -111,7 +124,7 @@ def run_mnist_experiment(model='avb', pretrained_model=None):
 
     latent_vars = trained_model.infer(train_data, batch_size=100, sampling_size=sampling_size)
     save_array(path_join(model_dir, 'latent_samples.npy'), latent_vars)
-    plot_latent_2d(latent_vars[:, -2:], repeat(train_data[0]['tag'], sampling_size),
+    plot_latent_2d(latent_vars[:, -2:], repeat(train_data[0]['target'], sampling_size),
                    fig_dirpath=model_dir, fig_name='shared.png')
     stop_id = 0
     for i, lat_id in enumerate(latent_dims[:-1]):
@@ -136,5 +149,4 @@ def run_mnist_experiment(model='avb', pretrained_model=None):
 
 
 if __name__ == '__main__':
-    run_synthetic_experiment(model='avb')#, pretrained_model='./output/conjoint_avb/synthetic/final')
-    # run_mnist_experiment(model='vae', pretrained_model='./output/conjoint_gaussian_vae/mnist_variations/final')
+    run_mnist_experiment(model='vae', two_backgrounds_per_encoder=True)
