f94016b6a9096e091cc28f69a8a995a252ef1daa
FHainzl <fab.hainzl@utanet.at>
2017-07-20 19:32:23 +0200

    Add structure for changed background experiment

--- launcher.py
+++ launcher.py
@@ -75,7 +75,7 @@ def run_synthetic_experiment(model='avb', pretrained_model=None):
 
 
 def run_mnist_experiment(model='avb', pretrained_model=None, two_backgrounds_per_encoder=True, color_tags=True,
-                         small_set=False):
+                         small_set=False, change_background=False, epochs=100):
     logger.info("Starting a conjoint model experiment on the MNIST Variations dataset.")
     data_dims = (784, 784)
     latent_dims = (2, 2, 2)
@@ -132,51 +132,57 @@ def run_mnist_experiment(model='avb', pretrained_model=None, two_backgrounds_per
     else:
         raise ValueError("Currently only `avb` and `vae` are supported.")
 
-    model_dir = trainer.run_training(train_data, batch_size=100, epochs=30, save_interrupted=True)
+    if change_background:
+        epochs = 0
+
+    model_dir = trainer.run_training(train_data, batch_size=100, epochs=epochs, save_interrupted=True)
     trained_model = trainer.get_model()
 
-    sampling_size = 1
+    if change_background:
+        pass
+    else:
+        sampling_size = 1
 
-    latent_vars = trained_model.infer(train_data, batch_size=100, sampling_size=sampling_size)
-    save_array(path_join(model_dir, 'latent_samples.npy'), latent_vars)
-    plot_latent_2d(latent_vars[:, -2:], repeat(train_data[0]['target'], sampling_size),
-                   fig_dirpath=model_dir, fig_name='shared.png')
+        latent_vars = trained_model.infer(train_data, batch_size=100, sampling_size=sampling_size)
+        save_array(path_join(model_dir, 'latent_samples.npy'), latent_vars)
+        plot_latent_2d(latent_vars[:, -2:], repeat(train_data[0]['target'], sampling_size),
+                       fig_dirpath=model_dir, fig_name='shared.png')
 
-    stop_id = 0
-    data = (data_0, data_1)
+        stop_id = 0
+        data = (data_0, data_1)
 
-    for i, lat_id in enumerate(latent_dims[:-1]):
-        start_id = stop_id
-        stop_id += lat_id
-        if color_tags:
-            tags = data[i]['tag'][:-test_size]
-            tags_num = zeros_like(train_data[0]['target'], dtype=np.int8)
-            tag_dict = dict(enumerate(unique(tags)))
-            assert len(tags_num) == len(tags)
-            for k, v in tag_dict.items():
-                tags_num[tags == v] = k
-            plot_latent_2d(latent_vars[:, start_id:stop_id], repeat(tags_num, sampling_size),
-                           fig_dirpath=model_dir, fig_name='private_{}'.format(i))
-        else:
-            plot_latent_2d(latent_vars[:, start_id:stop_id], repeat(train_data[i]['target'], sampling_size),
-                           fig_dirpath=model_dir, fig_name='private_{}'.format(i))
+        for i, lat_id in enumerate(latent_dims[:-1]):
+            start_id = stop_id
+            stop_id += lat_id
+            if color_tags:
+                tags = data[i]['tag'][:-test_size]
+                tags_num = zeros_like(train_data[0]['target'], dtype=np.int8)
+                tag_dict = dict(enumerate(unique(tags)))
+                assert len(tags_num) == len(tags)
+                for k, v in tag_dict.items():
+                    tags_num[tags == v] = k
+                plot_latent_2d(latent_vars[:, start_id:stop_id], repeat(tags_num, sampling_size),
+                               fig_dirpath=model_dir, fig_name='private_{}'.format(i))
+            else:
+                plot_latent_2d(latent_vars[:, start_id:stop_id], repeat(train_data[i]['target'], sampling_size),
+                               fig_dirpath=model_dir, fig_name='private_{}'.format(i))
 
-    reconstructions = trained_model.reconstruct(test_data, batch_size=100, sampling_size=1)
-    save_array(path_join(model_dir, 'reconstructed_samples.npy'), reconstructions)
-    for i, rec in enumerate(reconstructions):
-        plot_reconstructed_data(test_data[i]['data'], rec,
-                                fig_dirpath=model_dir, fig_name='reconstructed_{}'.format(i))
+        reconstructions = trained_model.reconstruct(test_data, batch_size=100, sampling_size=1)
+        save_array(path_join(model_dir, 'reconstructed_samples.npy'), reconstructions)
+        for i, rec in enumerate(reconstructions):
+            plot_reconstructed_data(test_data[i]['data'], rec,
+                                    fig_dirpath=model_dir, fig_name='reconstructed_{}'.format(i))
 
-    generations = trained_model.generate(n_samples=100, batch_size=100)
-    save_array(path_join(model_dir, 'generated_samples.npy'), generations)
-    for i, gen in enumerate(generations):
-        plot_sampled_data(gen, fig_dirpath=model_dir, fig_name='data_{}'.format(i))
+        generations = trained_model.generate(n_samples=100, batch_size=100)
+        save_array(path_join(model_dir, 'generated_samples.npy'), generations)
+        for i, gen in enumerate(generations):
+            plot_sampled_data(gen, fig_dirpath=model_dir, fig_name='data_{}'.format(i))
 
     clear_session()
     return model_dir
 
 
 if __name__ == '__main__':
-    # Two backgrounds per encoder (strippy horizontal/vertical and trippy/mandelbrot)
-    # Datasets (N=50k) are created at 'data/MNIST_Custom_Variations' if they don't exist (may take a while)
-    run_mnist_experiment(model='vae', two_backgrounds_per_encoder=True, color_tags=True)
+    pretrained_dir = "output/conjoint_gaussian_vae/vae_mnist_2ds_per_encoder_272epochs_fullsize"
+    run_mnist_experiment(model='vae', two_backgrounds_per_encoder=True, color_tags=True,
+                         pretrained_model=pretrained_dir)
