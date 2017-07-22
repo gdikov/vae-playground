f194198b409862fedf8984ea3c07be5b3c250775
Elli1993 <Eli.Wittmann@gmx.de>
2017-07-22 11:28:33 +0200

    only noise mode implementation for pull request

--- playground/models/networks/sampling.py
+++ playground/models/networks/sampling.py
@@ -1,4 +1,20 @@
-from keras.layers import Dense, Concatenate, Add
+from keras.layers import Dense, Concatenate, Add, Lambda, Reshape
+from keras import backend as K
+from numpy import newaxis
+
+
+def outer_product(inputs):
+    """
+    inputs: list of two tensors (of equal dimensions,
+        for which you need to compute the outer product
+    """
+    x, y = inputs
+    feature_size = x._shape_as_list()[1] ** 2
+    outer_Product = x[:,:, newaxis] * y[:,newaxis,:]
+    outer_Product = K.reshape(outer_Product, (-1, feature_size))
+    #outerProduct = K.reshape(outerProduct, (batchSize, -1))
+    # returns a flattened batch-wise set of tensors
+    return outer_Product
 
 
 def sample_standard_normal_noise(inputs, **kwargs):
@@ -23,6 +39,11 @@ def sample_standard_normal_noise(inputs, **kwargs):
         resized_noise = Dense(data_dim, activation=None, name='enc_resized_noise_sampler')(samples_isotropic)
         added_noise_data = Add(name='enc_adding_noise_data')([inputs, resized_noise])
         return added_noise_data
+    elif op_mode == 'product':
+        outputshape = noise_dim**2
+        bilinearProduct = Lambda(outer_product, output_shape=(noise_dim ** 2, ))([inputs, samples_isotropic])
+        # bilinearProduct = Reshape((outputshape),bilinearProduct)
+        return bilinearProduct
     return samples_isotropic
 
 
