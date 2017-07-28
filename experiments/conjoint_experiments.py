4df54cc7749482dbbbee653c137806cfc748be07
Georgi Dikov <gvdikov@gmail.com>
2017-07-29 01:38:37 +0300

    refactor experiments WIP1

---
+++ experiments/conjoint_experiments.py
@@ -0,0 +1,221 @@
+from __future__ import division
+
+from numpy import save as save_array
+from os.path import join as path_join, exists as path_exists
+from numpy import repeat, zeros
+from playground.utils.visualisation import plot_latent_2d, plot_sampled_data, plot_reconstructed_data
+from playground.model_trainer import ConjointVAEModelTrainer, ConjointAVBModelTrainer
+from playground.utils.datasets import load_conjoint_synthetic, load_mnist, join_datasets
+from playground.utils.data_factory import CustomMNIST
+from playground.utils.logger import logger
+from keras.backend import clear_session
+
+
+def synthetic(model='avb', pretrained_model=None, noise_mode='product'):
+    logger.info("Starting a conjoint model experiment on the synthetic dataset.")
+    data_dims = (8, 8)
+    latent_dims = (2, 2, 2)
+    data = load_conjoint_synthetic(dims=data_dims)
+
+    if model == 'vae':
+        trainer = ConjointVAEModelTrainer(data_dims=data_dims, latent_dims=latent_dims,
+                                          experiment_name='synthetic', architecture='synthetic',
+                                          overwrite=True, optimiser_params={'lr': 0.001},
+                                          pretrained_dir=pretrained_model)
+    elif model == 'avb':
+        trainer = ConjointAVBModelTrainer(data_dims=data_dims, latent_dims=latent_dims, noise_dim=data_dims[0],
+                                          use_adaptive_contrast=False,
+                                          optimiser_params={'encdec': {'lr': 1e-3, 'beta_1': 0.5},
+                                                            'disc': {'lr': 1e-3, 'beta_1': 0.5}},
+                                          schedule={'iter_discr': 3, 'iter_encdec': 1},
+                                          overwrite=True,
+                                          pretrained_dir=pretrained_model,
+                                          architecture='synthetic',
+                                          experiment_name='synthetic',
+                                          noise_mode=noise_mode)
+    else:
+        raise ValueError("{} model not supported. Choose between `avb` and `vae`.".format(model))
+
+    model_dir = trainer.run_training(data, batch_size=4, epochs=1000, save_interrupted=True)
+    # model_dir = './output/tmp'
+    trained_model = trainer.get_model()
+
+    sampling_size = 1000
+
+    latent_vars = trained_model.infer(data, batch_size=4, sampling_size=sampling_size)
+    save_array(path_join(model_dir, 'latent_samples.npy'), latent_vars)
+    plot_latent_2d(latent_vars[:, -2:], repeat(data[0]['target'], sampling_size),
+                   fig_dirpath=model_dir, fig_name='shared.png')
+    stop_id = 0
+    for i, lat_id in enumerate(latent_dims[:-1]):
+        start_id = stop_id
+        stop_id += lat_id
+        plot_latent_2d(latent_vars[:, start_id:stop_id], repeat(data[0]['tag'], sampling_size),
+                       fig_dirpath=model_dir, fig_name='private_{}'.format(i))
+
+    reconstructions = trained_model.reconstruct(data, batch_size=16, sampling_size=1)
+    save_array(path_join(model_dir, 'reconstructed_samples.npy'), reconstructions)
+    for i, rec in enumerate(reconstructions):
+        plot_reconstructed_data(data[i]['data'], rec,
+                                fig_dirpath=model_dir,
+                                fig_name='reconstructed_{}'.format(i),
+                                pad_to_size=16)
+
+    generations = trained_model.generate(n_samples=100, batch_size=100)
+    save_array(path_join(model_dir, 'generated_samples.npy'), generations)
+    for i, gen in enumerate(generations):
+        plot_sampled_data(gen, fig_dirpath=model_dir, fig_name='data_{}'.format(i))
+
+    clear_session()
+    return model_dir
+
+
+def mnist_variations(n_datasets=1, model='avb', pretrained_model=None):
+    return {1: mnist_variations_one(model=model, pretrained_model=pretrained_model),
+            2: mnist_variations_two(model=model, pretrained_model=pretrained_model)}[n_datasets]
+
+
+def mnist_variations_one(model='avb', pretrained_model=None):
+    logger.info("Starting a conjoint model experiment on the MNIST Variations "
+                "dataset (one dataset per autoencoder).")
+    data_dims = (784, 784)
+    latent_dims = (2, 2, 2)
+    data_0 = load_mnist(local_data_path='data/MNIST_Custom_Variations/strippy_horizontal.npz',
+                        one_hot=False, binarised=False, background='custom')
+    data_1 = load_mnist(local_data_path='data/MNIST_Custom_Variations/strippy_checker.npz',
+                        one_hot=False, binarised=False, background='custom')
+    train_data = ({'data': data_0['data'][:-100], 'target': data_0['target'][:-100], 'tag': data_0['tag'][:-100]},
+                  {'data': data_1['data'][:-100], 'target': data_1['target'][:-100], 'tag': data_1['tag'][:-100]})
+    test_data = ({'data': data_0['data'][-100:], 'target': data_0['target'][-100:], 'tag': data_0['tag'][-100:]},
+                 {'data': data_1['data'][-100:], 'target': data_1['target'][-100:], 'tag': data_1['tag'][-100:]})
+
+    if model == 'vae':
+        trainer = ConjointVAEModelTrainer(data_dims=data_dims, latent_dims=latent_dims,
+                                          experiment_name='mnist_variations_one', architecture='mnist',
+                                          overwrite=True, save_best=True,
+                                          optimiser_params={'lr': 0.0007, 'beta_1': 0.5},
+                                          pretrained_dir=pretrained_model)
+    elif model == 'avb':
+        trainer = ConjointAVBModelTrainer(data_dims=data_dims, latent_dims=latent_dims, noise_dim=64,
+                                          use_adaptive_contrast=False,
+                                          optimiser_params={'encdec': {'lr': 1e-3, 'beta_1': 0.5},
+                                                            'disc': {'lr': 2e-3, 'beta_1': 0.5}},
+                                          overwrite=True, save_best=True,
+                                          pretrained_dir=pretrained_model,
+                                          architecture='mnist',
+                                          experiment_name='mnist_variations_one')
+    else:
+        raise ValueError("Currently only `avb` and `vae` are supported.")
+
+    model_dir = trainer.run_training(train_data, batch_size=100, epochs=1000,
+                                     save_interrupted=True,
+                                     validation_data=test_data,
+                                     validation_frequency=20,
+                                     validation_sampling_size=5)
+    # model_dir = 'output/tmp'
+    trained_model = trainer.get_model()
+
+    sampling_size = 1
+
+    latent_vars = trained_model.infer(train_data, batch_size=100, sampling_size=sampling_size)
+    save_array(path_join(model_dir, 'latent_samples.npy'), latent_vars)
+    plot_latent_2d(latent_vars[:, -2:], repeat(train_data[0]['target'], sampling_size),
+                   fig_dirpath=model_dir, fig_name='shared.png')
+    stop_id = 0
+    for i, lat_id in enumerate(latent_dims[:-1]):
+        start_id = stop_id
+        stop_id += lat_id
+        plot_latent_2d(latent_vars[:, start_id:stop_id], repeat(train_data[0]['tag'], sampling_size),
+                       fig_dirpath=model_dir, fig_name='private_{}'.format(i))
+
+    reconstructions = trained_model.reconstruct(test_data, batch_size=100, sampling_size=1)
+    save_array(path_join(model_dir, 'reconstructed_samples.npy'), reconstructions)
+    for i, rec in enumerate(reconstructions):
+        plot_reconstructed_data(test_data[i]['data'], rec,
+                                fig_dirpath=model_dir, fig_name='reconstructed_{}'.format(i))
+
+    generations = trained_model.generate(n_samples=100, batch_size=100)
+    save_array(path_join(model_dir, 'generated_samples.npy'), generations)
+    for i, gen in enumerate(generations):
+        plot_sampled_data(gen, fig_dirpath=model_dir, fig_name='data_{}'.format(i))
+
+    clear_session()
+    return model_dir
+
+
+def mnist_variations_two(model='avb', pretrained_model=None, **kwargs):
+    logger.info("Starting a conjoint model experiment on the MNIST Variations "
+                "dataset (two datasets per autoencoder).")
+    data_dims = (784, 784)
+    latent_dims = (2, 2, 2)
+
+    datasets = kwargs.get('dataset_pairs', [('horizontal', 'trippy'), ('vertical', 'black')])
+    cmnist = CustomMNIST()
+    data = []
+    for dataset in datasets:
+        dataset_path = path_join('data', 'MNIST_Custom_Variations', '_'.join(dataset) + '.npz')
+        if not path_exists(dataset_path):
+            styles = {k: v for k, v in enumerate(cmnist.pattern_generator.get_styles())}
+            style_dist = zeros(len(list(styles.keys())))
+            custom_data = []
+            for s in dataset:
+                style_dist[styles[s]] = 1 / len(dataset)
+                custom_data.append(cmnist.augment(style_distribution=style_dist))
+            custom_data = join_datasets(custom_data, shuffle=True)
+            cmnist.save_dataset(custom_data, dataset_path)
+        data.append(load_mnist(local_data_path=dataset_path, one_hot=False, binarised=False, background='custom'))
+    train_data = tuple([{k: d[k][:-100] for k in d.keys()} for d in data])
+    test_data = tuple([{k: d[k][-100:] for k in d.keys()} for d in data])
+
+    if model == 'vae':
+        trainer = ConjointVAEModelTrainer(data_dims=data_dims, latent_dims=latent_dims,
+                                          experiment_name='mnist_variations', architecture='mnist',
+                                          overwrite=True, save_best=True,
+                                          optimiser_params={'lr': 0.0007, 'beta_1': 0.5},
+                                          pretrained_dir=pretrained_model)
+    elif model == 'avb':
+        trainer = ConjointAVBModelTrainer(data_dims=data_dims, latent_dims=latent_dims, noise_dim=64,
+                                          use_adaptive_contrast=False,
+                                          optimiser_params={'encdec': {'lr': 1e-3, 'beta_1': 0.5},
+                                                            'disc': {'lr': 2e-3, 'beta_1': 0.5}},
+                                          overwrite=True, save_best=True,
+                                          pretrained_dir=pretrained_model,
+                                          architecture='mnist',
+                                          experiment_name='mnist_variations')
+    else:
+        raise ValueError("Currently only `avb` and `vae` are supported.")
+
+    model_dir = trainer.run_training(train_data, batch_size=100, epochs=1000,
+                                     save_interrupted=True,
+                                     validation_data=test_data,
+                                     validation_frequency=20,
+                                     validation_sampling_size=5)
+    # model_dir = 'output/tmp'
+    trained_model = trainer.get_model()
+
+    sampling_size = 1
+
+    latent_vars = trained_model.infer(train_data, batch_size=100, sampling_size=sampling_size)
+    save_array(path_join(model_dir, 'latent_samples.npy'), latent_vars)
+    plot_latent_2d(latent_vars[:, -2:], repeat(train_data[0]['target'], sampling_size),
+                   fig_dirpath=model_dir, fig_name='shared.png')
+    stop_id = 0
+    for i, lat_id in enumerate(latent_dims[:-1]):
+        start_id = stop_id
+        stop_id += lat_id
+        plot_latent_2d(latent_vars[:, start_id:stop_id], repeat(train_data[0]['tag'], sampling_size),
+                       fig_dirpath=model_dir, fig_name='private_{}'.format(i))
+
+    reconstructions = trained_model.reconstruct(test_data, batch_size=100, sampling_size=1)
+    save_array(path_join(model_dir, 'reconstructed_samples.npy'), reconstructions)
+    for i, rec in enumerate(reconstructions):
+        plot_reconstructed_data(test_data[i]['data'], rec,
+                                fig_dirpath=model_dir, fig_name='reconstructed_{}'.format(i))
+
+    generations = trained_model.generate(n_samples=100, batch_size=100)
+    save_array(path_join(model_dir, 'generated_samples.npy'), generations)
+    for i, gen in enumerate(generations):
+        plot_sampled_data(gen, fig_dirpath=model_dir, fig_name='data_{}'.format(i))
+
+    clear_session()
+    return model_dir
