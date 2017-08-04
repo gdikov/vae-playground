3a43ebc7a002a027bf449514cdb5c943c0c23439
FHainzl <fab.hainzl@utanet.at>
2017-08-04 16:09:54 +0200

    Changes to main and _init_ to load model

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from experiments.launcher import run_from_code
-from experiments import mnist_variations, synthetic
-
+from experiments import mnist_variations, synthetic, change_background_save_latent
 
 if __name__ == '__main__':
     # run_from_code(experiment=synthetic, model='avb', pretrained_model=None, noise_mode='product')
-    run_from_code(experiment=mnist_variations, n_datasets=2, model='avb', pretrained_model=None)
+    # run_from_code(experiment=mnist_variations, n_datasets=2, model='avb', pretrained_model=None)
+    run_from_code(experiment=change_background_save_latent, model='vae',
+                  pretrained_model="output")
